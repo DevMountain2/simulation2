8a1b6a88b46bb75c8932388e170df213a2e9de63
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-11-03 15:25:58 -0500

    fixed homepage background  shrink issue

--- simulation2/src/components/Home/Home.js
+++ simulation2/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.png'
 import './Home.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class LoginForm extends Component {
     render() {
@@ -12,12 +12,12 @@ class LoginForm extends Component {
                         <img src={logo} alt="Houser Logo" />
                         <div className="login-fields-container">
                             <form className="login-fields">
-                                <div class="input-descr">Username</div>
+                                <div className="input-descr">Username</div>
                                 <input type="text"></input>
-                                <div class="input-descr">Password</div>
+                                <div className="input-descr">Password</div>
                                 <input type="text"></input>
                             </form>
-                            <Link   to="/dashboard"><button type="button" className="login-button"> Login </button></Link>
+                            <Link to="/dashboard"><button type="button" className="login-button"> Login </button></Link>
                             <button type="button" className="register-button"> Register </button>
                         </div>
                     </div>
