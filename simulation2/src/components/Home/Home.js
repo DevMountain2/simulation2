35272b19162b7964d63d1428b00977de7b27592c
Mario A. Hoyos <mariohoyos92@gmail.com>
2017-11-03 14:40:49 -0500

    added login functionality

--- simulation2/src/components/Home/Home.js
+++ simulation2/src/components/Home/Home.js
@@ -2,8 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.png'
 import './Home.css'
 import {Link} from 'react-router-dom'
+import axios from 'react'
 
 class LoginForm extends Component {
+    constructor(props){
+
+    
+    super(props)
+    this.state = {
+        username: "",
+        password: ""
+    }
+
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+}
+
+handleUsername(value){
+    this.setState({username: value})
+}
+
+handlePassword(value){
+    this.setState({password: value})
+}
+
+
+handleLogin(){
+    axios.post("http://localhost:3000/api/login",{username: this.state.username, password: this.state.password}).then((response) => console.log(response)).catch(console.log)
+}
+
+
+
     render() {
         return (
             <div>
@@ -13,11 +43,11 @@ class LoginForm extends Component {
                         <div className="login-fields-container">
                             <form className="login-fields">
                                 <div class="input-descr">Username</div>
-                                <input type="text"></input>
-                                <div class="input-descr">Password</div>
+                                <input type="text" onChange={(e) => this.handleLogin(e.target.value)}></input>
+                                <div class="input-descr" onChange={(e) => this.handlePassword(e.target.value)}>Password</div>
                                 <input type="text"></input>
                             </form>
-                            <Link   to="/dashboard"><button type="button" className="login-button"> Login </button></Link>
+                            <Link to="/dashboard" ><button type="button" className="login-button" onClick={this.handleLogin}> Login </button></Link>
                             <button type="button" className="register-button"> Register </button>
                         </div>
                     </div>
