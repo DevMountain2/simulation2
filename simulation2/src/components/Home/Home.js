7de672d6da269e39284b743b63d66fa054d0a359
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-11-03 13:31:24 -0500

    almost finished homeview commit

--- simulation2/src/components/Home/Home.js
+++ simulation2/src/components/Home/Home.js
@@ -6,17 +6,19 @@ class LoginForm extends Component {
     render() {
         return (
             <div>
-                <div className="container">
-                    <img src={logo} alt="Houser Logo" />
-                    <div className="login-fields-container">
-                        <form className="login-fields">
-                            <div class="input-descr">Username</div>
-                            <input type="text"></input>
-                            <div class="input-descr">Password</div>
-                            <input type="text"></input>
-                        </form>
-                        <button type="button"> Login </button>
-                        <button type="button"> Register </button>
+                <div className="main-container">
+                    <div className="main-content">
+                        <img src={logo} alt="Houser Logo" />
+                        <div className="login-fields-container">
+                            <form className="login-fields">
+                                <div class="input-descr">Username</div>
+                                <input type="text"></input>
+                                <div class="input-descr">Password</div>
+                                <input type="text"></input>
+                            </form>
+                            <button type="button" className="login-button"> Login </button>
+                            <button type="button" className="register-button"> Register </button>
+                        </div>
                     </div>
                 </div>
             </div >
